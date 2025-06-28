994cde7f3eadff78b10545f565c11435415b9d0e
Kim <kimniklas.schlueter@gmail.com>
2025-06-28 11:35:55 +0200

    Reorder projects in data file

--- src/app/data/projects.data.ts
+++ src/app/data/projects.data.ts
@@ -2,35 +2,12 @@ import { IProject } from '../models/project.model';
 
 export const PROJECTS: IProject[] = [
   {
-    name: 'iolite',
-    imageFilename: 'terrain_02.png',
-    gitHubLink: 'https://github.com/knsdev/iolite',
-    description:
-      'iolite is a simple and lightweight 3D game engine written in C++ with OpenGL. It includes a basic terrain editor, allowing you to create and modify 3D landscapes.',
-    features: [
-      '3D rendering with OpenGL',
-      'Terrain editor',
-      'Flying Camera',
-      'Loading model files (obj), textures (png, jpg), shaders (glsl)',
-      'Modular and extendable design',
-    ],
-    languages: ['C++', 'GLSL'],
-  },
-  {
-    name: 'Food blog',
-    imageFilename: 'food-blog-about.jpg',
-    gitHubLink: 'https://github.com/knsdev/food-blog',
-    description: 'First Exam at CodeFactory',
-    features: ['Responsive design'],
-    languages: ['HTML', 'CSS'],
-  },
-  {
-    name: 'Weekly Planner',
-    imageFilename: 'weekly-planner.jpg',
-    gitHubLink: 'https://github.com/knsdev/weekly-planner',
-    description: 'Second Exam at CodeFactory',
-    features: ['Responsive design'],
-    languages: ['HTML', 'CSS', 'JS'],
+    name: 'This Portfolio Website',
+    imageFilename: 'portfolio-website.png',
+    gitHubLink: 'https://github.com/knsdev/portfolio',
+    description: '',
+    features: ['Angular app', 'Responsive design'],
+    languages: ['HTML', 'CSS', 'TS'],
   },
   {
     name: 'Online Store Frontend',
@@ -46,11 +23,34 @@ export const PROJECTS: IProject[] = [
     languages: ['HTML', 'CSS', 'TS'],
   },
   {
-    name: 'This Portfolio Website',
-    imageFilename: 'portfolio-website.png',
-    gitHubLink: 'https://github.com/knsdev/portfolio',
-    description: '',
-    features: ['Angular app', 'Responsive design'],
-    languages: ['HTML', 'CSS', 'TS'],
+    name: 'Weekly Planner',
+    imageFilename: 'weekly-planner.jpg',
+    gitHubLink: 'https://github.com/knsdev/weekly-planner',
+    description: 'Second Exam at CodeFactory',
+    features: ['Responsive design'],
+    languages: ['HTML', 'CSS', 'JS'],
+  },
+  {
+    name: 'Food blog',
+    imageFilename: 'food-blog-about.jpg',
+    gitHubLink: 'https://github.com/knsdev/food-blog',
+    description: 'First Exam at CodeFactory',
+    features: ['Responsive design'],
+    languages: ['HTML', 'CSS'],
+  },
+  {
+    name: 'iolite',
+    imageFilename: 'terrain_02.png',
+    gitHubLink: 'https://github.com/knsdev/iolite',
+    description:
+      'iolite is a simple and lightweight 3D game engine written in C++ with OpenGL. It includes a basic terrain editor, allowing you to create and modify 3D landscapes.',
+    features: [
+      '3D rendering with OpenGL',
+      'Terrain editor',
+      'Flying Camera',
+      'Loading model files (obj), textures (png, jpg), shaders (glsl)',
+      'Modular and extendable design',
+    ],
+    languages: ['C++', 'GLSL'],
   },
 ];
