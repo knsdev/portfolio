385728f88967371f65b6e63d8bc2d8936c2e20dd
Kim <kimniklas.schlueter@gmail.com>
2025-06-28 15:45:45 +0200

    Make Navbar sticky and fix related issues

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { NgTemplateOutlet } from '@angular/common';
+
+const NAV_BREAKPOINT: number = 902;
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [NgTemplateOutlet],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
-export class Navbar {}
+export class Navbar {
+  collapsed: boolean = true;
+  isSmallScreen = window.innerWidth < NAV_BREAKPOINT;
+
+  toggleNavbar() {
+    this.collapsed = !this.collapsed;
+  }
+
+  @HostListener('window:resize', [])
+  onWindowResize() {
+    this.isSmallScreen = window.innerWidth < NAV_BREAKPOINT;
+
+    if (!this.isSmallScreen) {
+      this.collapsed = true;
+    }
+  }
+}
