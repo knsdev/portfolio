98e0ad2b296dd1eb771d6d3f247832a87b607a63
Kim <kimniklas.schlueter@gmail.com>
2025-06-29 18:12:27 +0200

    Add conditional styling to skill titles and update skill entries

--- src/app/skills/skills.ts
+++ src/app/skills/skills.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
-import { ISkill } from '../models/skill.model';
+import { ISkill, SkillLevel } from '../models/skill.model';
 import { SKILLS } from '../data/skills.data';
 import { getProjectById } from '../data/projects.data';
 import { ProjectId } from '../models/project.model';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './skills.html',
   styleUrl: './skills.css',
 })
 export class Skills {
   skills: ISkill[] = SKILLS;
+
+  SkillLevel = SkillLevel;
 
   getProjectById(id: ProjectId) {
     return getProjectById(id);
