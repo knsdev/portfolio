891e86b7ede041da80b7062b057ec738bc4255b2
Kim <kimniklas.schlueter@gmail.com>
2025-07-04 12:49:22 +0200

    Improve Navbar responsiveness and replace hrefs with scrollToSection(id)

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,7 +1,7 @@
 import { Component, HostListener } from '@angular/core';
 import { NgTemplateOutlet } from '@angular/common';
 
-const NAV_BREAKPOINT: number = 902;
+const NAV_BREAKPOINT: number = 903; // has to be 1px greater than the breakpoint in css
 
 @Component({
   selector: 'app-navbar',
@@ -23,6 +23,17 @@ export class Navbar {
 
     if (!this.isSmallScreen) {
       this.collapsed = true;
+    }
+  }
+
+  scrollToSection(id: string): void {
+    if (id) {
+      const el = document.getElementById(id);
+      if (el) {
+        el.scrollIntoView({ behavior: 'smooth' });
+      }
+    } else {
+      window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
     }
   }
 }
