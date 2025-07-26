07655e8fa875d3945b32300d81acaad95dd59770
Kim <kimniklas.schlueter@gmail.com>
2025-07-26 22:41:03 +0200

    Update projects data

--- src/app/data/projects.data.ts
+++ src/app/data/projects.data.ts
@@ -75,7 +75,7 @@ export const PROJECTS: IProject[] = [
     imageFilename: 'terrain_02.png',
     gitHubLink: 'https://github.com/knsdev/iolite',
     description:
-      'iolite is a simple and lightweight 3D game engine written in C++ with OpenGL. It includes a basic terrain editor, allowing you to create and modify 3D landscapes.',
+      'A simple and lightweight 3D game engine written in C++ with OpenGL. It includes a basic terrain editor, allowing you to create and modify 3D landscapes.',
     features: [
       '3D rendering with OpenGL',
       'Terrain editor',
